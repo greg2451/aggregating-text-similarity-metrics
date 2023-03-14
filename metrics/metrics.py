22a196af5c7dd3f88d792d1ddac2558fd0fde155
greg2451 <51173502+greg2451@users.noreply.github.com>
2023-03-14 22:38:05 +0100

    feat: make metrics return a float by text pair

--- metrics/metrics.py
+++ metrics/metrics.py
@@ -4,6 +4,7 @@ from typing import List, Dict
 from nlg_eval_via_simi_measures.bary_score import BaryScoreMetric
 from nlg_eval_via_simi_measures.depth_score import DepthScoreMetric
 from nlg_eval_via_simi_measures.infolm import InfoLM
+from tqdm import tqdm
 import evaluate
 
 
@@ -13,30 +14,57 @@ class Metric:
         raise NotImplementedError
 
     @abstractmethod
-    def __call__(self, *args, **kwargs):
+    def __call__(self, *args, **kwargs) -> List[float]:
         raise NotImplementedError
+
+
+SCORE_FIELD_NAME = {
+    "bleu": "bleu",
+    "chrf": "score",
+    "meteor": "meteor",
+    "bertscore": "f1",
+    "sacrebleu": "score",
+    "bary": "baryscore_W",
+    "depth": "depth_score",
+}
 
 
 class HuggingFaceMetric(Metric):
     def __init__(self, name: str):
+        self.name = name
         self.scorer = evaluate.load(name)
 
-    def __call__(self, references: List[str], predictions: List[str], *args, **kwargs):
-        if name == "bertscore":
+    def __call__(
+        self, references: List[str], predictions: List[str], *args, **kwargs
+    ) -> List[float]:
+        if self.name == "bertscore":
             # BERTScore should be called specifying the language or the model type, so we default to English
             if "lang" not in kwargs and "model_type" not in kwargs:
                 kwargs["model_type"] = "distilbert-base-uncased"
 
-        return self.scorer.compute(
-            references=[[ref] for ref in references],
-            predictions=predictions,
-            *args,
-            **kwargs,
-        )
+            print("Computing BERTScore...")
+            bert_scores = self.scorer.compute(
+                references=[[reference] for reference in references],
+                predictions=predictions,
+                *args,
+                **kwargs,
+            )
+            return bert_scores[SCORE_FIELD_NAME[self.name]]
+
+        return [
+            self.scorer.compute(
+                references=[[reference]],
+                predictions=[prediction],
+                *args,
+                **kwargs,
+            )[SCORE_FIELD_NAME[self.name]]
+            for reference, prediction in tqdm(zip(references, predictions, strict=True))
+        ]
 
 
 class NLGEvalViaSimilarityMeasuresMetric(Metric):
     def __init__(self, name: str, *args, **kwargs):
+        self.name = name
         if name == "bary":
             self.scorer = BaryScoreMetric(*args, **kwargs)
         elif name == "depth":
@@ -48,9 +76,13 @@ class NLGEvalViaSimilarityMeasuresMetric(Metric):
                 f"Unknown metric {name}, expected one of bary, depth, infolm"
             )
 
-    def __call__(self, references: List[str], predictions: List[str]):
+    def __call__(self, references: List[str], predictions: List[str]) -> List[float]:
         self.scorer.prepare_idfs(references, predictions)
-        return self.scorer.evaluate_batch(references, predictions)
+        return self.scorer.evaluate_batch(references, predictions)[
+            self.scorer.measure_to_use
+            if self.name == "infolm"
+            else SCORE_FIELD_NAME[self.name]
+        ]
 
 
 def load_all_metrics() -> Dict[str, Metric]:
