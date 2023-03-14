f749f1889d4f30498dee3a3d679567662d31c68d
greg2451 <51173502+greg2451@users.noreply.github.com>
2023-03-14 23:11:23 +0100

    add setup.py to make it pip installable

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="aggregating-text-similarity-metrics",
+    version="0.0.1",
+    url="https://github.com/greg2451/aggregating-text-similarity-metrics.git",
+    author="Grégoire Retourné & Hugo Peltier",
+    author_email="gregoire.retourne@gmail.com",
+    description="Aggregating text similarity metrics",
+    install_requires=[],
+    packages=find_packages(),
+)
