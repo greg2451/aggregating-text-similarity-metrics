e215b860f5087d17642589e992a0e568ef25ebad
greg2451 <51173502+greg2451@users.noreply.github.com>
2023-03-15 21:04:36 +0100

    package: drop pre-req and update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,30 @@
 from setuptools import setup, find_packages
 
+import os
+
+
+DIR = os.path.realpath(os.path.dirname(__file__))
+
+
+with open(os.path.join(DIR, "requirements.txt")) as f:
+    requirements = [line.strip() for line in f.readlines()]
+
+
+with open(os.path.join(DIR, "README.md"), encoding="utf-8") as readme_file:
+    readme = readme_file.read()
+
 
 setup(
     name="aggregating-text-similarity-metrics",
     version="0.0.1",
     url="https://github.com/greg2451/aggregating-text-similarity-metrics.git",
-    author="Grégoire Retourné & Hugo Peltier",
+    author="Grégoire Retourné, Hugo Peltier",
     author_email="gregoire.retourne@gmail.com",
-    description="Aggregating text similarity metrics",
-    install_requires=[],
+    short_description="Aggregating text similarity metrics",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    python_requires=">=3.9",
+    install_requires=requirements,
     packages=find_packages(),
+    keywords=["natural language processing", "text similarity", "metrics aggregation"],
 )
