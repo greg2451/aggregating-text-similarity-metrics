5d64edafcfde0fadbe6c702068867f22b6eed65a
greg2451 <51173502+greg2451@users.noreply.github.com>
2023-03-17 15:58:50 +0100

    feat: final version on WMT script

--- experiment/WMT.py
+++ experiment/WMT.py
@@ -1,21 +1,45 @@
+import os
+
 from metrics import load_all_metrics
 from benchmark_datasets import get_wmt_data
 import pandas as pd
+from datetime import datetime
+
+DIR = os.path.dirname(os.path.realpath(__file__))
 
 if __name__ == "__main__":
+    # Create result dir
+    now = str(datetime.now())[:19]
+    os.makedirs(os.path.join(DIR, f"results/run_{now}"), exist_ok=True)
+
+    # Load dataset and metrics
     dataset = get_wmt_data()
     metrics = load_all_metrics()
     results = pd.DataFrame()
     results["human_scores"] = dataset.human_scores
+
+    # Loop over metrics
     for name, metric in metrics.items():
-        results[name] = metric(dataset.references, dataset.candidates)
-        results.to_csv("results.csv", index=False)
+        scores = metric(dataset.references, dataset.candidates)
+        for sub_metric_name, sub_score in scores.items():
+            results[sub_metric_name] = sub_score
 
-    # Pearson correlation coefficient
-    results.corr(method="pearson")
+        # Save checkpoint, this is very useful in case of crash
+        results.to_csv(
+            os.path.join(DIR, f"results/run_{now}/checkpoint.csv"), index=False
+        )
 
-    # Spearman correlation coefficient
-    results.corr(method="spearman")
+    # Save final result and delete checkpoints
+    columns_in_right_order = ["human_scores"] + [
+        column for column in sorted(results.columns) if column != "human_scores"
+    ]
+    results = results.reindex(columns_in_right_order, axis=1)
+    results.to_csv(os.path.join(DIR, f"results/run_{now}/final.csv"), index=False)
+    os.remove(os.path.join(DIR, f"results/run_{now}/checkpoint.csv"))
 
-    # Kendall's tau
-    results.corr(method="kendall")
+    # Compute correlations and save them
+    os.makedirs(os.path.join(DIR, f"results/run_{now}/correlations"), exist_ok=True)
+    for correlation_type in ["pearson", "spearman", "kendall"]:
+        results.corr(method=correlation_type).to_csv(
+            os.path.join(DIR, f"results/run_{now}/correlations/{correlation_type}.csv")
+        )
