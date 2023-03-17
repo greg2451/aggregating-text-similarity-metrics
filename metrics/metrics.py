29be9788adb3b2a953a3603843f1de863064b4b3
greg2451 <51173502+greg2451@users.noreply.github.com>
2023-03-17 15:57:27 +0100

    refactor: new metrics

--- metrics/metrics.py
+++ metrics/metrics.py
@@ -14,7 +14,7 @@ class Metric:
         raise NotImplementedError
 
     @abstractmethod
-    def __call__(self, *args, **kwargs) -> List[float]:
+    def __call__(self, *args, **kwargs) -> Dict[str, List[float]]:
         raise NotImplementedError
 
 
@@ -24,6 +24,7 @@ SCORE_FIELD_NAME = {
     "meteor": "meteor",
     "bertscore": "f1",
     "sacrebleu": "score",
+    "ter": "score",
     "bary": "baryscore_W",
     "depth": "depth_score",
 }
@@ -36,7 +37,7 @@ class HuggingFaceMetric(Metric):
 
     def __call__(
         self, references: List[str], predictions: List[str], *args, **kwargs
-    ) -> List[float]:
+    ) -> Dict[str, List[float]]:
         assert len(references) == len(
             predictions
         ), "The number of references and predictions should be the same"
@@ -52,17 +53,34 @@ class HuggingFaceMetric(Metric):
                 *args,
                 **kwargs,
             )
-            return bert_scores[SCORE_FIELD_NAME[self.name]]
+            return {self.name: bert_scores[SCORE_FIELD_NAME[self.name]]}
 
-        return [
-            self.scorer.compute(
-                references=[[reference]],
-                predictions=[prediction],
-                *args,
-                **kwargs,
-            )[SCORE_FIELD_NAME[self.name]]
-            for reference, prediction in tqdm(zip(references, predictions))
-        ]
+        if self.name == "rouge":
+            # Return all rouge metrics since they are computed at the same time!
+            scores = [
+                self.scorer.compute(
+                    references=[[reference]],
+                    predictions=[prediction],
+                    *args,
+                    **kwargs,
+                )
+                for reference, prediction in tqdm(zip(references, predictions))
+            ]
+            return {
+                field: [score[field] for score in scores] for field in scores[0].keys()
+            }
+
+        return {
+            self.name: [
+                self.scorer.compute(
+                    references=[[reference]],
+                    predictions=[prediction],
+                    *args,
+                    **kwargs,
+                )[SCORE_FIELD_NAME[self.name]]
+                for reference, prediction in tqdm(zip(references, predictions))
+            ]
+        }
 
 
 class NLGEvalViaSimilarityMeasuresMetric(Metric):
@@ -81,41 +99,53 @@ class NLGEvalViaSimilarityMeasuresMetric(Metric):
                 f"Unknown metric {name}, expected one of bary, depth, infolm"
             )
 
-    def __call__(self, references: List[str], predictions: List[str]) -> List[float]:
+    def __call__(
+        self, references: List[str], predictions: List[str]
+    ) -> Dict[str, List[float]]:
         assert len(references) == len(
             predictions
         ), "The number of references and predictions should be the same"
         self.scorer.prepare_idfs(references, predictions)
-        return self.scorer.evaluate_batch(references, predictions)[
+        key = (
             self.scorer.measure_to_use
             if self.name == "infolm"
             else SCORE_FIELD_NAME[self.name]
-        ]
+        )
+        return {self.name: self.scorer.evaluate_batch(references, predictions)[key]}
 
 
-def load_all_metrics() -> Dict[str, Metric]:
-    return {
+def load_all_metrics(only_fast: bool = True) -> Dict[str, Metric]:
+    metrics = {
         "bleu": HuggingFaceMetric("bleu"),
         "chrf": HuggingFaceMetric("chrf"),
         "meteor": HuggingFaceMetric("meteor"),
-        "bertscore": HuggingFaceMetric("bertscore"),
         "sacrebleu": HuggingFaceMetric("sacrebleu"),
-        "bary": NLGEvalViaSimilarityMeasuresMetric("bary"),
-        "depth": NLGEvalViaSimilarityMeasuresMetric("depth"),
-        "infolm": NLGEvalViaSimilarityMeasuresMetric("infolm"),
+        "ter": HuggingFaceMetric("ter"),
     }
+
+    if not only_fast:
+        metrics = {
+            **metrics,
+            "rouge": HuggingFaceMetric("rouge"),
+            "bertscore": HuggingFaceMetric("bertscore"),
+            "bary": NLGEvalViaSimilarityMeasuresMetric("bary"),
+            "depth": NLGEvalViaSimilarityMeasuresMetric("depth"),
+            "infolm": NLGEvalViaSimilarityMeasuresMetric("infolm"),
+        }
+
+    return metrics
 
 
 if __name__ == "__main__":
     import json
 
-    metrics = load_all_metrics()
+    all_metrics = load_all_metrics()
 
     references = ["I like my cakes very much", "I hate these cakes!"]
     predictions = ["I adore my cakes", "These cakes are bad!"]
 
     scores = {}
-    for name, metric in metrics.items():
+    for name, metric in all_metrics.items():
         scores[name] = metric(references, predictions)
 
     print(json.dumps(scores, indent=2))
